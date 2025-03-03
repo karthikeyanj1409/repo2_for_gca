25a346fda8b13737aaea8f7cab937b6d6c3dfe34
karthikeyanj1409 <144699980+karthikeyanj1409@users.noreply.github.com>
2025-03-03 03:07:52 +0000

    Add files via upload

---
+++ codefile2.ts
@@ -0,0 +1,20 @@
+export function Function6()
+{
+    return "Function6 from codefile2.ts";
+}
+export function Function7()
+{
+    return "Function7 from codefile2.ts";
+}
+export function Function8()
+{
+    return "Function8 from codefile2.ts";
+}
+export function Function9()
+{
+    return "Function9 from codefile2.ts";
+}
+export function Function10()
+{
+    return "Function10 from codefile2.ts";
+}
